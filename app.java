7b62e92a1c675e87f4d1660df0d1c63c4a71459a
alejandro19102 <159305253+alejandro19102@users.noreply.github.com>
2025-03-22 17:11:00 +0100

    Update app.java

--- app.java
+++ app.java
@@ -1,12 +1,38 @@
 import java.io.File;
 
 public class app{
+  //funcion main
   public static void main(String[]args){
-  String configFilePath = "configuracion.txt";
-    File configFile = new File(configFilePath);
+    // Declaraciond de variables
+  String nombreArchivo = "configuracion.txt";
+    File archivo = new File(configFilePath);
     if (configFile.exist()){
       System.out.println("El archivo configuracion ya exixte");
-      return;}
-   System.out.println("El archivo no existe");
+      return;}else{
+      try {
+        if (archivo.createNewFile()) {
+        System.out.println("Archivo '" + nombreArchivo + "' creado correctamente.");
+    } // Crear los directorios requeridos
+                crearDirectorio("jugador");
+                crearDirectorio("escenario");
+                crearDirectorio("juego");
+            } catch (IOException e) {
+                System.out.println("Error al crear el archivo: " + e.getMessage());
+            }
   }
+}
+  //Funcion para crear directorios introduciendo el nombre
+  private static void crearDirectorio(String nombreDirectorio) {
+    //Declaracion de Directorio
+        File directorio = new File(nombreDirectorio);
+        if (!directorio.exists()) {
+            if (directorio.mkdir()) {
+                System.out.println("Directorio '" + nombreDirectorio + "' creado correctamente.");
+            } else {
+                System.out.println("Error al crear el directorio '" + nombreDirectorio + "'.");
+            }
+        } else {
+            System.out.println("El directorio '" + nombreDirectorio + "' ya existe.");
+        }
+    }
 }
