5a7fff102a4930da6f1317c945ea2ddbe1693473
guilleriveraa <guilleriveraa12@gmail.com>
2025-03-22 17:02:19 +0100

    Update app.java

--- app.java
+++ app.java
@@ -1,1 +1,12 @@
+import java.io.File;
 
+public class app{
+  public static void main(String[]args){
+  String configFilePath = "configuracion.txt";
+    File configFile = new File(configFilePath);
+    if (configFile.exist()){
+      System.out.println("El archivo configuracion ya exixte");
+      return;}
+   System.out.println("El archivo no existe");
+  }
+}
