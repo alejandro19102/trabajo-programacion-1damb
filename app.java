cbd317560f3629a879048d33cad85d8634cc4b82
alejandro19102 <159305253+alejandro19102@users.noreply.github.com>
2025-03-26 16:20:37 +0100

    Update app.java

--- app.java
+++ app.java
@@ -1,29 +1,28 @@
 import java.io.File;
 
-public class app{
-  //funcion main
-  public static void main(String[]args){
-    // Declaraciond de variables
-  String nombreArchivo = "configuracion.txt";
-    File archivo = new File(nombreArchivo);
-    if (archivo.exist()){
-      System.out.println("El archivo configuracion ya exixte");
-      return;}else{
-      try {
-        if (archivo.createNewFile()) {
-        System.out.println("Archivo '" + nombreArchivo + "' creado correctamente.");
-    } // Crear los directorios requeridos
+public class App{
+    //funcion main
+    public static void main(String[]args){
+        String nombreArchivo = "configuracion.txt";
+        File archivo = new File(nombreArchivo);
+        if (archivo.exists()){
+            System.out.println("El archivo configuracion ya exixte");
+            return;}else{
+            try {
+                if (archivo.createNewFile()) {
+                    System.out.println("Archivo '" + nombreArchivo + "' creado correctamente.");
+                } // Crear los directorios requeridos
                 crearDirectorio("jugador");
                 crearDirectorio("escenario");
                 crearDirectorio("juego");
-            } catch (IOException e) {
+            } catch (Exception e) {
                 System.out.println("Error al crear el archivo: " + e.getMessage());
             }
-  }
-}
-  //Funcion para crear directorios introduciendo el nombre
-  private static void crearDirectorio(String nombreDirectorio) {
-    //Declaracion de Directorio
+        }
+    }
+    //Funcion para crear directorios introduciendo el nombre
+    private static void crearDirectorio(String nombreDirectorio) {
+        //Declaracion de Directorio
         File directorio = new File(nombreDirectorio);
         if (!directorio.exists()) {
             if (directorio.mkdir()) {
